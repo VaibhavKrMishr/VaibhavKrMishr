013951820024b829415e6854fa8c3f7e81fd8188
Vaibhav Kr. Mishra <79004538+VaibhavKrMishr@users.noreply.github.com>
2022-08-21 07:21:21 +0530

    Update Eater (Game).py

--- Eater (Game).py
+++ Eater (Game).py
@@ -1,6 +1,8 @@
 import pygame
 pygame.init()
 import random
+import tkinter
+from tkinter.constants import *
 
 white = (255, 255, 255)
 red = (255, 0, 0)
@@ -8,9 +10,6 @@ black = (0, 0, 0)
 blue  = (0,0,255)
 height = 400
 width = 600
-
-import tkinter
-from tkinter.constants import *
 
 
 tk = tkinter.Tk()
@@ -32,7 +31,7 @@ clock = pygame.time.Clock()
 font =  pygame.font.SysFont(None, 20)
 
 with open("High_Score.txt", "r") as h:
-    hs = h.read()
+    High_Score = h.read()
 
 def text_screen(test, color, x ,y):
     screen_score = font.render(test, True, color)
@@ -52,11 +51,12 @@ def gameloop():
     food_size = eater_size
     score = 0
     fps = 30
+    global High_Score 
     
     while not exit_game:
         if game_over:
-            with open("hiscore.txt", "w") as f:
-                f.write(str(hiscore))
+            with open("High_Score.txt", "w") as h:
+                h.write(str(High_Score))
              
             gamewindow.fill(white)
             text_screen("Game Over!!! Press Enter To Continue", red, width/4, height/4)
@@ -105,10 +105,15 @@ def gameloop():
                         
             gamewindow.fill(white)
             text_screen("Score: " + str(score), blue ,5,5)
+            text_screen("High Score: "+ str(High_Score), blue ,width-150,5)
+            
             
             if abs(eater_x - food_x)<5 and (eater_y - food_y)<5:
                 score = score + 1
-                print(hs)
+            
+                if score>int(High_Score):
+                    High_Score = score 
+    
                 food_x = random.randint(0,width)
                 food_y = random.randint(0,height)
          
@@ -121,15 +126,9 @@ def gameloop():
 
             pygame.draw.rect(gamewindow ,black, [eater_x, eater_y, eater_size, eater_size])
             pygame.draw.rect(gamewindow, red, [food_x, food_y ,food_size/2 , food_size/2])
+        pygame.init()
         pygame.display.update()
-        clock.tick(fps)
-       
+        clock.tick(fps)     
     pygame.quit()
-
-
 gameloop()
            
-
-
-
-
